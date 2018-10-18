24a5f254765242df3fb6f04efa1b8c2fd6d79a4c
altaras0v <44142716+altaras0v@users.noreply.github.com>
2018-10-18 15:20:03 +0300

    Add files via upload
    Upload to 2 classes

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+             Console.WriteLine("If your data is more than one word, please use quotes \" \"\n");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string maxSequenceLength = MaxUniqueSequnceFinder.MaxFinderMethod(args[i]);
+                Console.WriteLine("Your data: " + args[i]);
+                Console.WriteLine(maxSequenceLength);
+            }
+        }     
+    }
+}
