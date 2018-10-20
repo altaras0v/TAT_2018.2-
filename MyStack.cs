a49cab77595ea2d3d8ab8de59e63828305c02f37
altaras0v <44142716+altaras0v@users.noreply.github.com>
2018-10-20 19:47:02 +0300

    Stack

---
+++ MyStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassTask1
+{
+    class MyStack
+    {
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        static void Main(string[] args)
+        {
+           
+                Stack myStack = new Stack(10);                // create stack with max.size = 10
+                for (int i = 0; i < 9; i++)                   //fill the stack
+               {
+                    myStack.push(i + 5);
+                }
+                   
+                if (!(myStack.emptyStack()))             
+                myStack.pop();                                // delete element
+
+                Console.WriteLine("MyStack is ");             // output stack
+                if (myStack.emptyStack())
+                {
+                    Console.WriteLine(("MyStack is empty"));
+
+                }
+                else
+                {
+                    while (!(myStack.emptyStack()))            //check, that stack is not empty
+                    {
+
+                        int stackElement = myStack.pop();
+                        Console.WriteLine(stackElement);
+
+                    }
+
+                }
+                Console.Read();  
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,56 @@
+﻿
+namespace ClassTask1
+{ 
+    /// <summary>
+    ///  Class, that realize stack and its methods
+    /// </summary>
+    class Stack
+    {
+        public int maxSize;
+        public int[] masStack;                     // array,that the basis of stack
+        public int size;                           // size of stack
+        /// <summary>
+        ///  constructor, that realizes stack  
+        /// </summary>
+        /// <param name="maxSize">
+        /// takes the maximum size ofstack
+        /// </param>
+        public Stack(int maxSize)
+        {
+            this.maxSize = maxSize;
+            masStack = new int[maxSize];
+            size = -1;
+        }
+        /// <summary>
+        /// Method that adds elemnts in stack
+        /// </summary>
+        /// <param name="value">
+        /// Value that is added to the stack
+        /// </param>
+        public void push(int value)
+        {
+           size = size + 1; 
+            masStack[size] = value;
+        }
+        /// <summary>
+        /// Method that removes an element
+        /// </summary>
+        /// <returns>
+        /// Return stack without last element
+        /// </returns>
+        public int pop()
+        {  
+            return  (masStack[size--]);
+        }
+        /// <summary>
+        /// Method,that checks does stack has elements
+        /// </summary>
+        /// <returns> 
+        /// If stack empty return true
+        /// </returns>
+        public bool emptyStack()
+        {
+            return (size==-1);
+        }
+    }
+}
