28a51c2ccc3a796e6cd5d0d4b162a8280bbf5c49
altaras0v <44142716+altaras0v@users.noreply.github.com>
2018-10-23 23:47:24 +0300

    Add files via upload

---
+++ ProgramConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task3
+{
+    /// <summary>
+    /// Program that converts number from one numeral system to another
+    /// </summary>
+    class ProgramConverter
+    {
+        /// <summary>
+        /// Entry point of programm 
+        /// </summary>
+        /// <param name="args">Two arguments (Integer number and new numeral base) from command line 
+        /// that entered by user </param>
+        static void Main(string[] args)
+        {
+           try
+           {
+               int newBase = int.Parse(args[1]);                                            
+               if (newBase < 2 || newBase > 20)                                               //Сhecking range of the base of a new number system
+               {
+                    Console.WriteLine(("Error in base of numeral system."));
+               }
+                RadixConverter convertNumber = new RadixConverter();
+                Console.WriteLine(convertNumber.Converter(args[0],newBase));
+           }
+           catch (FormatException)
+           {
+              Console.WriteLine("You need to write an integer number");
+           }
+           
+           catch (Exception ex)
+           {
+                Console.WriteLine("Exception: " + ex.Message);
+           }         
+        }
+    }
+}
---
+++ RadixConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task3
+{
+    /// <summary>
+    /// Class that converts number from one numeral system to another
+    /// </summary>
+    class RadixConverter
+    {
+        private int oldBase = 10;
+        /// <summary>
+        /// Method that converts number from one numeral system to another
+        /// </summary>
+        /// <param name="nubmer">Integer number that entered by user</param>
+        /// <param name="newBase">Base of new numeral system that entered by user </param>
+        /// <returns>Number in new numeral system</returns>
+        public string Converter(string nubmer, int newBase)
+        {
+            //Converts a string representation of a number with the specified base of a number system 
+            //to its equivalent 32-bit integer
+            return (Convert.ToString(Convert.ToInt32(nubmer, oldBase), newBase)); 
+        }
+    }
+}
