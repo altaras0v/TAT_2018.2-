69f4355fe674628c633c8a3b9dccd52fac2e99b6
altaras0v <44142716+altaras0v@users.noreply.github.com>
2018-12-11 16:31:35 +0300

    Add files via upload

---
+++ WebDriver/WebDriver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriverTask
+{
+    class WebDriver
+    {
+        public void WebDrive()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/login/");
+            var elementEmail = driver.FindElement(By.Id("email"));
+            var elementPassword = driver.FindElement(By.Id("pass"));
+            var loginButton = driver.FindElement(By.Id("loginbutton"));
+
+
+            elementEmail.SendKeys("altaras0b@gmail.com");
+            elementPassword.SendKeys("idNM7x01");
+            loginButton.Click();
+
+            driver.Navigate().GoToUrl("https://www.facebook.com/messages/");
+
+            var unreadMessages = driver.FindElementsByClassName("_1ht5 _2il3 _5l-3 _3itx");
+
+        }
+    }
+}
